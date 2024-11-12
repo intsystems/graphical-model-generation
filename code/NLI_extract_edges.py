1c6aae6b8efd3fcbe01914c713e50b228f9af354
Ernest Nasyrov <2001092236@mail.ru>
2024-11-12 17:39:26 +0300

    wrote files with sync and async functions

--- code/NLI_extract_edges.py
+++ code/NLI_extract_edges.py
@@ -1,154 +1,217 @@
-"""Code for verticies extraction.
+"""Code for vertices extraction.
+
+This module provides functionality to extract edges between nodes in a graph using OpenAI's GPT models. It includes methods to determine the existence and direction of edges based on a textual description.
 
 The module contains the following functions:
 
-- `extract_one_edge_gpt` - Returns
-- `extract_all_edges` - Returns
+- _extract_one_edge_gpt: Determines the existence and direction of an edge between a pair of nodes.
+- extract_all_edges: Extracts all possible edges from a set of nodes based on a given description.
 """
 
 
-from openai import OpenAI, AsyncOpenAI
 from pydantic import BaseModel
 from enum import Enum
-import os
-from langchain_openai import ChatOpenAI
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-from langchain.prompts import PromptTemplate, HumanMessagePromptTemplate
 from openai import OpenAI, AsyncOpenAI
-from typing import List, Tuple
-import json
+from NLI_base_extractor import BaseExtractior
+from config import node_extraction_sys_message, edge_extraction_str_template
+
 
 class ArrowEnum(str, Enum):
+    """
+        Enumeration for arrow direction types in a graph.
+    """
     no = "no arrow"
     forward = "forward arrow"
     backward = "backward arrow"
 
+
 class ArrowType(BaseModel):
+    """
+        Model representing the type of arrow direction for an edge.
+    """
     arrow_type: ArrowEnum
 
-def get_messages_for_edge_direction(descr: str, set_of_nodes: List[str], pair_of_nodes: Tuple[str]) -> str:
 
-    node_extraction_sys_message = '''
-            ###ROLE###
-            You are Graphical model scientist.
-            You task is to extract information about DAG from text description.
-            The structure, or topology, of the network should capture qualitative relationships between variables. In particular, two nodes should be connected directly if one affects or causes the other, with the arc indicating the direction of the effect.
-            The presence of arrows or arcs seems to imply, at an intuitive level, that for each arc one variable should be interpreted as a cause and the other as an effect (e.g., A →E means that A causes E). This interpretation is called causal.
+class EdgeExtractor(BaseExtractior):
+    """
+        Class to handle edge extraction from a description using OpenAI's GPT models.
+    """
 
-            For every right answer I give you 5$.
-        '''
+    def __init__(self, openai_client: OpenAI|AsyncOpenAI) -> None:
+        """
+            Initializes the NodeExtraction with an OpenAI client.
 
-    edge_extraction_str_template = f'''
-        ###TASK###
-        You are given a DAG description, a set of its nodes and pair of nodes.
-        You should infer from the description and you own knowledge the type of casuality between two given nodes:
-            - forward: the left may be the cause of the right
-            - backward: the right may be the cause of the left
-            - no: no direct casualities
+            Args:
+                openai_client (OpenAI | AsyncOpenAI): An instance of OpenAI or AsyncOpenAI client to interact with the API.
+        """
+        super(EdgeExtractor, self).__init__(openai_client)
 
-        ###OUTPUT FORMAT###
-        You output should be "forward", "backward" or "no".
+    
+    @staticmethod
+    def _get_messages_for_edge_direction(description: str, 
+                                         set_of_nodes: list[str], 
+                                         pair_of_nodes: tuple[str, str],
+    ) -> tuple[dict, dict]:
+        """
+            Prepares the message payload for edge direction identification using the GPT model.
 
-        ###
+            Args:
+                description (str): The description text to be processed.
+                set_of_nodes (list[str]): The nodes of the graph.
+                pair_of_nodes (tuple[str, str]): Pair of nodes to decide about the existance and the direction of the edge.
 
-        User:
-            #DESCRIPTION#: Imagine a garden where the growth of plants depends on several factors. The amount of Watering affects how well the plants grow. Sunlight is another crucial factor, as it provides energy for photosynthesis. Fertilizer also plays a role by supplying essential nutrients. Together, these factors influence Plant Growth. Additionally, Watering can impact the effectiveness of Fertilizer, as nutrients are better absorbed when the soil is moist.
-            #SET OF NODES#: [Watering, Sunlight, Fertilizer, Plant Growth]
-            #PAIR OF NODES#: (Sunlight, Plant Growth)
+            Returns:
+                tuple[dict, dict]: A tuple containing system and user messages formatted for the API request.
+        """  
 
-        Assistant: forward
+        messages = [
+            {
+                'role': 'system',
+                'content': node_extraction_sys_message
+            },
+            {
+                'role': 'user',
+                'content': edge_extraction_str_template.format(description=description, set_of_nodes=set_of_nodes, pair_of_nodes=pair_of_nodes)
+            }
+        ]
 
-        ###
-
-        User:
-            #DESCRIPTION#: Imagine a garden where the growth of plants depends on several factors. The amount of Watering affects how well the plants grow. Sunlight is another crucial factor, as it provides energy for photosynthesis. Fertilizer also plays a role by supplying essential nutrients. Together, these factors influence Plant Growth. Additionally, Watering can impact the effectiveness of Fertilizer, as nutrients are better absorbed when the soil is moist.
-            #SET OF NODES#: [Watering, Sunlight, Fertilizer, Plant Growth]
-            #PAIR OF NODES#: (Fertilizer, Watering)
-
-        Assistant: backward
-
-        ###
-
-        User:
-            #DESCRIPTION#: Imagine a garden where the growth of plants depends on several factors. The amount of Watering affects how well the plants grow. Sunlight is another crucial factor, as it provides energy for photosynthesis. Fertilizer also plays a role by supplying essential nutrients. Together, these factors influence Plant Growth. Additionally, Watering can impact the effectiveness of Fertilizer, as nutrients are better absorbed when the soil is moist.
-            #SET OF NODES#: [Watering, Sunlight, Fertilizer, Plant Growth]
-            #PAIR OF NODES#: (Sunlight, Fertilizer)
-
-        Assistant: no
-
-        ###
-
-        User:
-            #DESCRIPTION#: {descr}
-            #SET OF NODES#: {set_of_nodes}
-            #PAIR OF NODES#: {pair_of_nodes}
-
-        Assistant:
-    '''
+        return messages
 
 
-    messages = [
-        {
-            'role': 'system',
-            'content': node_extraction_sys_message
-        },
-        {
-            'role': 'user',
-            'content': edge_extraction_str_template
-        }
-    ]
+    def _extract_one_edge_gpt(self,
+                             description: str, 
+                             set_of_nodes: list[str],
+                             pair_of_nodes: tuple[str, str], 
+                             gpt_model: str = 'gpt-4o-mini',
+                             temperature: float = 0,
+    ) -> tuple[str|None, str|None]:
+        """
+            Determines the existence and direction of an edge between a pair of nodes using a GPT model.
 
-    return messages
+            Args:
+                description (str): The description text to be processed.
+                set_of_nodes (list[str]): The nodes of the graph.
+                pair_of_nodes (tuple[str, str]): A pair of nodes to check for an edge.
+                gpt_model (str, optional): The GPT model to use for extraction. Defaults to 'gpt-4o-mini'.
+                temperature (float, optional): The randomness of the model's output. Defaults to 0.
 
-def extract_one_edge_gpt(descr, set_of_nodes, pair_of_nodes, gpt_client, gpt_model='gpt-4o-mini', temperature=0):
-    '''
-        returns either (None, None) or edge with identified direction
-    '''
-    completion = gpt_client.beta.chat.completions.parse(
-        model=gpt_model,
-        messages=get_messages_for_edge_direction(descr, f"[{', '.join(set_of_nodes)}]", f"[{', '.join(pair_of_nodes)}]"),
-        response_format=ArrowType,
-        temperature=temperature,
-    )
-    arrow_type = json.loads(completion.choices[0].message.content)['arrow_type']  # json.loads(completion.choices[0].message.content)['list_of_nodes']
+            Returns:
+                tuple[str | None, str | None]: either (None, None) if no edge exists, or a tuple representing the edge with identified direction.
+        """
 
-    if 'forward' in arrow_type.lower():
-        return pair_of_nodes
-    if 'backward' in arrow_type.lower():
-        return pair_of_nodes[::-1]
-    return (None, None)
+        arrow_type = self._get_completion_parsed_result(
+            model=gpt_model,
+            messages=self._get_messages_for_edge_direction(description, f"[{', '.join(set_of_nodes)}]", f"[{', '.join(pair_of_nodes)}]"),
+            response_format=ArrowType,
+            temperature=temperature,
+            key='arrow_type'
+        )
 
-# AsyncOpenAI
-async def async_extract_one_edge_gpt(descr, set_of_nodes, pair_of_nodes, async_gpt_client, gpt_model='gpt-4o-mini', temperature=0):
-    '''
-        returns either (None, None) or edge with identified direction
-    '''
-    completion = await async_gpt_client.beta.chat.completions.parse(
-        model=gpt_model,
-        messages=get_messages_for_edge_direction(descr, f"[{', '.join(set_of_nodes)}]", f"[{', '.join(pair_of_nodes)}]"),
-        response_format=ArrowType,
-        temperature=temperature,
-    )
-    arrow_type = json.loads(completion.choices[0].message.content)['arrow_type']  # json.loads(completion.choices[0].message.content)['list_of_nodes']
+        if 'forward' in arrow_type.lower():
+            return pair_of_nodes
+        if 'backward' in arrow_type.lower():
+            return pair_of_nodes[::-1]
+        
+        return (None, None)
+    
 
-    if 'forward' in arrow_type.lower():
-        return pair_of_nodes
-    if 'backward' in arrow_type.lower():
-        return pair_of_nodes[::-1]
-    return (None, None)
+    def extract_all_edges(self,
+                                description: str, 
+                                set_of_nodes: list[str],
+                                gpt_model: str = 'gpt-4o-mini',
+                                temperature: float = 0,
+                                verbose=False
+    ) -> tuple[str|None, str|None]:
+        """
+            Extracts all possible edges from a set of nodes based on a given description.
 
-async def extract_all_edges(descr, set_of_nodes, async_gpt_client, gpt_model='gpt-4o-mini', temperature=0):
-    edge_list = []
+            Args:
+                description (str): The description text to be processed.
+                set_of_nodes (list[str]): The nodes of the graph.
+                gpt_model (str, optional): The GPT model to use for extraction. Defaults to 'gpt-4o-mini'.
+                temperature (float, optional): The randomness of the model's output. Defaults to 0.
+                verbose (bool, optional): If True, provides detailed logging. Defaults to False.
 
-    for i, node_a in enumerate(set_of_nodes):
-        for node_b in set_of_nodes[i+1:]:
-            print(f"{node_a} # {node_b}")
-            edge = await async_extract_one_edge_gpt(descr, set_of_nodes, (node_a, node_b), async_gpt_client, gpt_model=gpt_model, temperature=temperature)
-            if edge[0] is not None:
-                edge_list.append(edge)
+            Returns:
+                list[tuple[str | None, str | None]]: A list of tuples representing edges with identified directions.
+        """    
+        edge_list = []
 
-    return edge_list
+        for i, node_a in enumerate(set_of_nodes):
+            for node_b in set_of_nodes[i+1:]:
+                if verbose:
+                    print(f"{node_a} # {node_b}")
+                edge = self._extract_one_edge_gpt(description, set_of_nodes, (node_a, node_b), gpt_model, temperature)
+                if edge[0] is not None:
+                    edge_list.append(edge)
+
+        return edge_list
+    
+
+    async def _async_extract_one_edge_gpt(self,
+                                    description: str, 
+                                    set_of_nodes: list[str],
+                                    pair_of_nodes: tuple[str, str], 
+                                    gpt_model: str = 'gpt-4o-mini',
+                                    temperature: float = 0,
+    ) -> tuple[str|None, str|None]:
+        """
+            Asynchronously extracts a directed edge between a pair of nodes using a specified GPT model.
+
+            Args:
+                description (str): The text description from which the edge needs to be extracted.
+                set_of_nodes (List[str]): A list of node names to consider for edge extraction.
+                pair_of_nodes (Tuple[str, str]): A tuple representing the pair of nodes to evaluate for an edge.
+                gpt_model (str, optional): The GPT model to use for extraction. Defaults to 'gpt-4o-mini'.
+                temperature (float, optional): The randomness of the model's output. Defaults to 0.0.
+
+            Returns:
+                Tuple[Optional[str], Optional[str]]: A tuple representing the directed edge or (None, None) if no edge is identified.
+        """
+        arrow_type = await self._async_get_completion_parsed_result(
+            model=gpt_model,
+            messages=self._get_messages_for_edge_direction(description, f"[{', '.join(set_of_nodes)}]", f"[{', '.join(pair_of_nodes)}]"),
+            response_format=ArrowType,
+            temperature=temperature,
+            key='arrow_type'
+        )
+
+        if 'forward' in arrow_type.lower():
+            return pair_of_nodes
+        if 'backward' in arrow_type.lower():
+            return pair_of_nodes[::-1]
+        
+        return (None, None)
+    
+
+    async def async_extract_all_edges(self,
+                                description: str, 
+                                set_of_nodes: list[str],
+                                gpt_model: str = 'gpt-4o-mini',
+                                temperature: float = 0,
+                                verbose=False
+    ) -> tuple[str|None, str|None]:
+        """
+            Asynchronously extracts all possible directed edges between nodes in a set using a specified GPT model.
+
+            Args:
+                description (str): The text description from which edges need to be extracted.
+                set_of_nodes (List[str]): A list of node names to consider for edge extraction.
+                gpt_model (str, optional): The GPT model to use for extraction. Defaults to 'gpt-4o-mini'.
+                temperature (float, optional): The randomness of the model's output. Defaults to 0.0.
+                verbose (bool, optional): If True, prints each node pair being processed. Defaults to False.
+
+            Returns:
+                List[Tuple[Optional[str], Optional[str]]]: A list of tuples representing directed edges between nodes.
+        """    
+        edge_list = []
+
+        for i, node_a in enumerate(set_of_nodes):
+            for node_b in set_of_nodes[i+1:]:
+                if verbose:
+                    print(f"{node_a} # {node_b}")
+                edge = await self._async_extract_one_edge_gpt(description, set_of_nodes, (node_a, node_b), gpt_model, temperature)
+                if edge[0] is not None:
+                    edge_list.append(edge)
+
+        return edge_list
--- code/NLI_node_extraction.py
+++ code/NLI_node_extraction.py
@@ -1,70 +1,74 @@
-
-import os
-from google.colab import userdata
-from langchain_openai import ChatOpenAI
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-from langchain.prompts import PromptTemplate, HumanMessagePromptTemplate
-from openai import OpenAI, AsyncOpenAI
 from typing import List
 from pydantic import BaseModel
-import json
-
-def get_messages_for_node_extraction(descr: str) -> str:
-
-    node_extraction_sys_message = '''
-            ###ROLE###
-            You are Graphical model scientist.
-            You task is to extract information about DAG from text description.
-            The structure, or topology, of the network should capture qualitative relationships between variables. In particular, two nodes should be connected directly if one affects or causes the other, with the arc indicating the direction of the effect.
-            The presence of arrows or arcs seems to imply, at an intuitive level, that for each arc one variable should be interpreted as a cause and the other as an effect (e.g., A →E means that A causes E). This interpretation is called causal.
-
-            For every right answer I give you 5$.
-        '''
-
-    node_extraction_str_template = f'''
-        ###TASK###
-        Extract all node names of GAD from the description, the user gaves you.
-
-        ###OUTPUT FORMAT###
-        You output should be list of strings
-
-        ###
-
-        User: Imagine a garden where the growth of plants depends on several factors. The amount of Watering affects how well the plants grow. Sunlight is another crucial factor, as it provides energy for photosynthesis. Fertilizer also plays a role by supplying essential nutrients. Together, these factors influence Plant Growth. Additionally, Watering can impact the effectiveness of Fertilizer, as nutrients are better absorbed when the soil is moist.
-        Assistant: ['Watering', 'Sunlight', 'Fertilizer', 'Plant Growth']
-
-        ###
-
-        User: {descr}
-        Assistant:
-    '''
-
-
-    messages = [
-        {
-            'role': 'system',
-            'content': node_extraction_sys_message
-        },
-        {
-            'role': 'user',
-            'content': node_extraction_str_template
-        }
-    ]
-
-    return messages
+from openai import OpenAI, AsyncOpenAI
+from config import node_extraction_str_template, node_extraction_sys_message
+from NLI_base_extractor import BaseExtractior
 
 class Nodes(BaseModel):
     list_of_nodes: List[str]
 
-def extract_nodes_gpt(descr, gpt_client, gpt_model='gpt-4o-mini', temperature=0):
-    completion = gpt_client.beta.chat.completions.parse(
-        model=gpt_model,
-        messages=get_messages_for_node_extraction(descr),
-        response_format=Nodes,
-        temperature=temperature,
-    )
-    return json.loads(completion.choices[0].message.content)['list_of_nodes']
+class NodeExtractor(BaseExtractior):
+    """
+        A class to handle node extraction from a given text description using OpenAI's GPT models.
+    """
+
+    def __init__(self, openai_client: OpenAI|AsyncOpenAI) -> None:
+        """
+            Initializes the NodeExtractor with an OpenAI client.
+
+            Args:
+                openai_client (OpenAI | AsyncOpenAI): An instance of OpenAI or AsyncOpenAI client to interact with the API.
+        """
+        super(NodeExtractor, self).__init__(openai_client)
+
+    def extract_nodes_gpt(self,
+                          description: str,
+                          gpt_model: str = 'gpt-4o-mini', 
+                          temperature: float = 0.0
+    ) -> list[str]:
+        """
+            Extracts nodes from a given description using a specified GPT model.
+
+            Args:
+                description (str): The text description from which nodes need to be extracted.
+                gpt_model (str, optional): The GPT model to use for extraction. Defaults to 'gpt-4o-mini'.
+                temperature (float, optional): The randomness of the model's output. Defaults to 0.0.
+
+            Returns:
+                list[str]: A list of extracted nodes from the description.
+        """        
+        
+        list_of_nodes = self._get_completion_parsed_result(
+            model=gpt_model,
+            messages=self._get_messages_for_node_extraction(description),
+            response_format=Nodes,
+            temperature=temperature,
+            key='list_of_nodes'
+        )
+
+        return list_of_nodes
+    
+
+    @staticmethod
+    def _get_messages_for_node_extraction(description: str) -> tuple[dict, dict]:
+        """
+            Prepares the message payload for node extraction using the GPT model.
+
+            Args:
+                description (str): The description text to be processed.
+
+            Returns:
+                tuple[dict, dict]: A tuple containing system and user messages formatted for the API request.
+        """        
+        messages = (
+            {
+                'role': 'system',
+                'content': node_extraction_sys_message
+            },
+            {
+                'role': 'user',
+                'content': node_extraction_str_template.format(description=description)
+            }
+        )
+
+        return messages
